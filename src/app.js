5565343041ccdd6d04ba08d6f3d4f24393c166fd
Ahriane Ramos <76123138+misspx12@users.noreply.github.com>
2021-02-21 12:22:09 -0800

    created link for celsius and fahrenheit
    updated css and js

--- src/app.js
+++ src/app.js
@@ -30,11 +30,14 @@ function displayTemperature(response) {
   let feelsLike = document.querySelector("#feels-like");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+
+  celsiusTemp = response.data.main.temp;
+
   location.innerHTML = response.data.name;
   currentDate.innerHTML = formatDate(response.data.dt * 1000);
   description.innerHTML = response.data.weather[0].description;
   tempIcon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  temperature.innerHTML = Math.round(response.data.main.temp);
+  temperature.innerHTML = Math.round(celsiusTemp);
   feelsLike.innerHTML = Math.round(response.data.main.feels_like);
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
@@ -42,7 +45,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "d181817faaf7ac4148d91ac2cdf0c65a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -52,5 +55,30 @@ function formSubmit(event) {
   search(cityInput.value);
 }
 
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  celsius.classList.remove("active");
+  fahrenheit.classList.add("active");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  temperature.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(celsiusTemp);
+}
+
+search("San Francisco");
+
 let form = document.querySelector("#search-engine");
 form.addEventListener("submit", formSubmit);
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", displayFahrenheit);
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", displayCelsius);
